8580825ec6a22b8b14831c8d3fc3c62b94dd430d
[Portia Mthethwa] <you@example.com>
2024-03-13 13:39:31 +0200

    replaced dummie data with real info from the api

--- src/index.js
+++ src/index.js
@@ -49,6 +49,12 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
 function getForecast(city) {
   let apiKey = "282f8036ef5dat2effb4fc9cbocda23a";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -59,23 +65,26 @@ function displayForecast(response) {
 
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
  <div class="weather-forecast-day">
-   <div class="weather-forecast-date">${day}</div>
-   <div class="weather-forecast-icon">⛅</div>
+   <div class="weather-forecast-date">${formatDay(day.time)}</div>
+   <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
    <div class="weather-forecast-temperatures">
      <span class="weather-forecast-temperature">
-       <strong>15°</strong>
+       <strong>${Math.round(day.temperature.maximum)}°</strong>
      </span>
-     <span class="weather-forecast-temperature">9°</span>
+     <span class="weather-forecast-temperature-min">${Math.round(
+       day.temperature.minimum
+     )}°</span>
    </div>
  </div>
  `;
+    }
   });
   forecastElement.innerHTML = forecastHtml;
 }
